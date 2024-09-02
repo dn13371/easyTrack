6ddbdb2b053a6e05db3771daf90197035ed3b4ea
dn1337 <hehehewang1@gmail.com>
2024-09-02 16:30:21 +0200

    create sql alchemy

---
+++ db.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    userid = db.Column(db.Integer, primary_key=True)
+    mail = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password, method='pbkdf2:sha256')
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
---
+++ services/db_services.py
@@ -0,0 +1,16 @@
+from db import db, User 
+from faker import Faker
+
+def create(db):   
+    db.create_all()
+
+def populate(db):
+    fake = Faker()
+    for _ in range(10):  
+        user = User(
+            mail=fake.email(),
+            username=fake.user_name(),
+            password="password",  
+        )
+        db.session.add(user)
+    db.session.commit()
