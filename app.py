9a48bf302d9520fb2aa2649713ed41b098d88cf6
dn1337 <hehehewang1@gmail.com>
2024-09-06 14:21:23 +0200

    added login page

--- app.py
+++ app.py
@@ -10,17 +10,18 @@ app.config.from_object(DevConfig)
 
 db.init_app(app)
 
-@app.route('/index')
-def index():
-
-    return "asd"
-
+#CLI commands
 @app.cli.command('init-db')
 def init_db(): 
     db_services.create(db)
     db_services.populate(db)
 
-
+#Routes 
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET': 
+        return render_template('base.html')
+    if request.method == 'POST'
+        mail = request.form.get('email')
 if __name__ == '__main__':
-
     app.run()
